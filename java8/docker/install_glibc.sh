fdd841228afa5b8e852e03a51f3a33994d93ed5e
haolun <haolun@example.com>
2018-05-21 23:42:58 +0800

    update build scripts

--- java8/docker/install_glibc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#if [ -z "${build_fileserver}" ]; then build_fileserver="https://github.com"; fi
-if [ -z "${build_fileserver}" ]; then build_fileserver="http://o9wbz99tz.bkt.clouddn.com"; fi
-curl -Ls ${build_fileserver}/sgerrand/alpine-pkg-glibc/releases/download/2.23-r3/glibc-2.23-r3.apk > /tmp/glibc-2.23-r3.apk \
-    && apk add --allow-untrusted /tmp/glibc-2.23-r3.apk
